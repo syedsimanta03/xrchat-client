2b766d874a2d072e02cc715dbd63e22c8d238d45
sam <syed.simanta06@gmail.com>
2020-05-26 17:19:01 +0600

    onlyloaders

---
+++ components/ui/Loader/MulticolorsLoader/index.tsx
@@ -0,0 +1,12 @@
+import './style.scss'
+
+const Loader = () => {
+  return (
+    <div>
+      <div className='loader'></div>
+      <span className='text'>Loading...</span>
+    </div>
+  )
+}
+
+export default Loader
